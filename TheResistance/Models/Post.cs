866adc1b7beb7605c3f774110a39ec0fd912a302
jeanettie <jeanettienicole@gmail.com>
2017-07-18 13:39:47 -0400

    trying again

---
+++ TheResistance/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheResistance.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.DateCreated = DateTime.Now;
+
+
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Link { get; set; }
+        public DateTime DateCreated { get; private set; }
+
+       // public virtual File Files { get; set; }
+
+    }
+
+}
